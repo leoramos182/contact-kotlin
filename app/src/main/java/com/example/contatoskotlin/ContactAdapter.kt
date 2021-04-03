d5a9145551138e1f81b90f350e43acb03804823e
leoramos182 <leo.ramos182@hotmail.com>
2021-04-03 12:18:40 -0300

    Finalizando aula 3

--- app/src/main/java/com/example/contatoskotlin/ContactAdapter.kt
+++ app/src/main/java/com/example/contatoskotlin/ContactAdapter.kt
@@ -1,4 +1,36 @@
 package com.example.contatoskotlin
 
-class ContactAdapter(val contactList : ArrayList<Contact>) {
+import android.provider.ContactsContract
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
+
+class ContactAdapter(val contactList : ArrayList<Contact>) : RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_view,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapter.ViewHolder, position: Int) {
+        holder.bindItem(contactList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return contactList.size
+    }
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItem(contact : Contact){
+            val textName = itemView.findViewById<TextView>(R.id.contact_name)
+            val textPhone = itemView.findViewById<TextView>(R.id.contact_phone)
+
+            textName.text = contact.nome
+            textPhone.text = contact.phone
+        }
+    }
+
 }
